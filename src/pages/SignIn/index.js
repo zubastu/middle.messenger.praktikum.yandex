fe07db00ce6d741931a84c7c115d862b7ac29fdb
zubastu <79757684+zubastu@users.noreply.github.com>
2023-03-14 15:48:47 +0300

    Изменил версию ноды, добавил отправку билда через сервер

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,24 @@
 import Handlebars from "handlebars";
 import * as classes from "./styles.module.css";
-import {Form} from "../../partials/Form/Form";
-import {Input} from "../../partials/Input/Input";
-import {Submit} from "../../partials/Submit/Submit";
+
+import { Form } from "../../partials/Form/Form";
+import { Input } from "../../partials/Input/Input";
+import { Submit } from "../../partials/Submit/Submit";
 
 const children = [
-    Input('text', 'Логин', 'login'),
-    Input('password', 'Пароль', 'password'),
-    Submit('Авторизоваться'),
-]
+  Input("text", "Логин", "login"),
+  Input("password", "Пароль", "password"),
+  Submit("Авторизоваться"),
+];
 
 export const SignIn = () => {
-    return Handlebars.compile(`
+  return Handlebars.compile(`
         <section>
-           ${Form('Вход', {href: '/sign-up', text: 'Нет аккаунта?'}, children)()}
+           ${Form(
+             "Вход",
+             { href: "/sign-up", text: "Нет аккаунта?" },
+             children
+           )()}
         </section>
-    `)()
-}
+    `)();
+};
