9576e5939e5f4f1ba62dd893aab61d80758e8f99
zubastu <79757684+zubastu@users.noreply.github.com>
2023-03-14 15:41:27 +0300

    Изменил версию ноды, добавил отправку билда через сервер

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 3000
+const PORT = 3000;
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + "/dist"));
+
+app.get("/*", (req, res) => {
+  res.sendFile("index.html", { root: __dirname + "/dist/" });
+});
 
 app.listen(PORT, () => {
-    console.log(`App started in ${PORT} port`)
-})
+  console.log(`App started in ${PORT} port`);
+});
